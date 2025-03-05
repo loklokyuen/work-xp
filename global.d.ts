324f1f3afb851fbe95b6693e0e1b2ea50291f948
loklokyuen <loklokyuen.m@gmail.com>
2025-03-05 12:29:08 +0000

    add new types and interfaces

--- global.d.ts
+++ global.d.ts
@@ -8,13 +8,55 @@ declare global {
     interface accountProps {
         setIsNewUser: React.Dispatch<React.SetStateAction<boolean>>;
     }
-    type AccountType = "Student" | "Business" | "Guest" | null;
-    interface OpportunityCardProps {
+    interface Business {
+        uid: string;
+        displayName: string;
+        sector: string;
+        photoUrl: string;
+        email: string;
+        address: string;
+        county: string;
+        description: string;
+        opportunities: Opportunity[];
+        reviews: Review[];
+        applications: Application[];
+    }
+    interface Opportunity {
+        id: string;
         availability: string[];
         description: string;
         jobRole: string;
-        id: string;
+    }
+    interface Review {
+        rating: number;
+        review: string;
+        author: string; 
+    }
+    interface Student {
+        uid: string;
+        displayName: string;
+        photoUrl: string;
+        email: string;
+        applications: Application[];
+        reviews: Review[];
+        subjects: string[];
+        experience: Experience[];
+        county: string;
+        personalStatement: string;
+    }
+    interface Experience {
+        role: string;
+        employer: string;
+        startDate: string;
+        endDate: string;
+        description: string;
+    }
+    interface Application {
+        opportunityId: string;
+        businessId: string;
+        studentId: string;
+        status: string;
+        applicationMessage: string;
     }
 }
-
 export {};
