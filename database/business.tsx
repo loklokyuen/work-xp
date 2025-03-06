89cd8df62d8331da0f981b0332a8b54891560350
lawaffy <laurawathen@hotmail.com>
2025-03-06 11:25:16 +0000

    allow a user to filter by county & sector

--- database/business.tsx
+++ database/business.tsx
@@ -1,40 +1,65 @@
-import { collection, addDoc, getDocs, getFirestore, doc, getDoc, query, where } from "firebase/firestore";
+import {
+  collection,
+  addDoc,
+  getDocs,
+  getFirestore,
+  doc,
+  getDoc,
+  query,
+  where,
+} from "firebase/firestore";
 import { db } from "./firebase";
-const BusinessUsersCollection = collection(db, 'Business');
-
+const BusinessUsersCollection = collection(db, "Business");
 
 async function getBusinessById(uid: string): Promise<User> {
-    const docRef = doc(db, 'Business', uid);
-    const docSnap = await getDoc(docRef);
-    const business = docSnap.data();
-    return business as User;
+  const docRef = doc(db, "Business", uid);
+  const docSnap = await getDoc(docRef);
+  const business = docSnap.data();
+  return business as User;
 }
 
 async function getBusinesses(): Promise<Business[]> {
-    const querySnapshot = await getDocs(BusinessUsersCollection);
-    const businessesList = querySnapshot.docs.map((doc) => {
-        return { uid: doc.id, ...doc.data() } as Business;
-    }
-    );
-    return businessesList;
+  const querySnapshot = await getDocs(BusinessUsersCollection);
+  const businessesList = querySnapshot.docs.map((doc) => {
+    return { uid: doc.id, ...doc.data() } as Business;
+  });
+  return businessesList;
 }
 
 async function getBusinessBySector(sector: string): Promise<Business[]> {
-    const q = query(BusinessUsersCollection, where("sector", "==", sector));
-    const querySnapshot = await getDocs(q);
-    const businessesList = querySnapshot.docs.map((doc) => {
-        return { uid: doc.id, ...doc.data() } as Business;
-    });
-    return businessesList;
+  const q = query(BusinessUsersCollection, where("sector", "==", sector));
+  const querySnapshot = await getDocs(q);
+  const businessesList = querySnapshot.docs.map((doc) => {
+    return { uid: doc.id, ...doc.data() } as Business;
+  });
+  return businessesList;
 }
 
 async function getBusinessOpportunities(uid: string): Promise<Opportunity[]> {
-   const subCollectionRef = collection(doc(db, 'Business', uid), 'Opportunities');
-   const querySnapshot = await getDocs(subCollectionRef);
-   const opportunitiesList = querySnapshot.docs.map((doc) => {
-       return { id: doc.id, ...doc.data() } as Opportunity;
-   });
-   return opportunitiesList;
+  const subCollectionRef = collection(
+    doc(db, "Business", uid),
+    "Opportunities"
+  );
+  const querySnapshot = await getDocs(subCollectionRef);
+  const opportunitiesList = querySnapshot.docs.map((doc) => {
+    return { id: doc.id, ...doc.data() } as Opportunity;
+  });
+  return opportunitiesList;
 }
 
-export { getBusinessById, getBusinesses, getBusinessBySector, getBusinessOpportunities };
+async function getBusinessByCounty(county: string): Promise<Business[]> {
+  const q = query(BusinessUsersCollection, where("county", "==", county));
+  const querySnapshot = await getDocs(q);
+  const businessesList = querySnapshot.docs.map((doc) => {
+    return { uid: doc.id, ...doc.data() } as Business;
+  });
+  return businessesList;
+}
+
+export {
+  getBusinessById,
+  getBusinesses,
+  getBusinessBySector,
+  getBusinessOpportunities,
+  getBusinessByCounty,
+};
