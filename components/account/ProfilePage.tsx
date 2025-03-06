12bcfdad4f5ee3db8e7d7bab1c0b655a4e23ff32
Amitbalter <amitbaltern@gmail.com>
2025-03-06 22:43:20 +0000

    fixed imports after pull

--- components/account/ProfilePage.tsx
+++ components/account/ProfilePage.tsx
@@ -11,102 +11,109 @@ import { EditableStudentInfo } from "./EditableStudentInfo";
 import { auth } from "@/database/firebase";
 import styles from "@/app/styles";
 import { updatePassword } from "firebase/auth";
-import ImageViewer from "../imageViewer";
+import ImageViewer from "../expoComponents/imageViewer";
 import AvatarPickingModal from "@/modal/AvatarPickingModal";
 import { ChangePasswordModal } from "@/modal/ChangePasswordModal";
 
 export default function ProfilePage() {
-    const [loading, setLoading] = useState<Boolean>(true)
-    const [editButtonPressed, setEditButtonPressed] = useState<Boolean>(false)
-    const [businessInfo, setBusinessInfo] = useState<Business>()
-    const [studentInfo, setStudentInfo] = useState<Student>()
-    const [openAvatarPicker, setOpenAvatarPicker] = useState<boolean>(false)
-    const [openChangePassword, setOpenChangePassword] = useState<boolean>(false)
-    const {user, setUser, accountType, setAccountType} = useUserContext()
+    const [loading, setLoading] = useState<Boolean>(true);
+    const [editButtonPressed, setEditButtonPressed] = useState<Boolean>(false);
+    const [businessInfo, setBusinessInfo] = useState<Business>();
+    const [studentInfo, setStudentInfo] = useState<Student>();
+    const [openAvatarPicker, setOpenAvatarPicker] = useState<boolean>(false);
+    const [openChangePassword, setOpenChangePassword] = useState<boolean>(false);
+    const { user, setUser, accountType, setAccountType } = useUserContext();
     const placeHolderImage = require("@/assets/images/background-image.png");
 
     useEffect(() => {
-      if (!user) return;
-      if(accountType === "Business"){
-        getBusinessById(user.uid).then((res) => {
-          if (res) setBusinessInfo(res)
-          setLoading(false)
-          })
-      } else if (accountType === "Student"){
-        getStudentById(user.uid).then((res) => {
-          if (res) setStudentInfo(res)
-          setLoading(false)
-        })
-      }
-    }, [user])
+        if (!user) return;
+        if (accountType === "Business") {
+            getBusinessById(user.uid).then((res) => {
+                if (res) setBusinessInfo(res);
+                setLoading(false);
+            });
+        } else if (accountType === "Student") {
+            getStudentById(user.uid).then((res) => {
+                if (res) setStudentInfo(res);
+                setLoading(false);
+            });
+        }
+    }, [user]);
 
     const handleLogout = () => {
-      auth.signOut()
-      .then(() => {
-          setUser(null);
-          setAccountType(null);
-      })
-      .catch(() => {
-          // setError(error.message);
-      });
+        auth.signOut()
+            .then(() => {
+                setUser(null);
+                setAccountType(null);
+            })
+            .catch(() => {
+                // setError(error.message);
+            });
     };
     const handleChangePassword = (newPassword: string) => {
         const user = auth.currentUser;
         if (user) {
             updatePassword(user, newPassword)
-            .then(() => {
-                alert("Password changed successfully!");
-            })
-            .catch((error) => {
-                // setError(error.message);
-            });
+                .then(() => {
+                    alert("Password changed successfully!");
+                })
+                .catch((error) => {
+                    // setError(error.message);
+                });
         }
     };
-    
 
-    if (loading) return <Text>Loading...</Text>
+    if (loading) return <Text>Loading...</Text>;
     return (
-      <SafeAreaView style={styles.container}>
-        <ScrollView contentContainerStyle={styles.scrollViewContent}>
-          <Text style={styles.title}>Profile</Text>
-          <View style={{ alignItems: "flex-end", marginRight: 20, position: "absolute", right: 10, top: 10 }}>
-            <Button
-              mode="contained-tonal"
-              onPress={() => {setEditButtonPressed(!editButtonPressed)}}
-            >
-              {editButtonPressed ? "Go back" : "Edit"}
-            </Button>
-          </View>
-          {editButtonPressed? null:
+        <SafeAreaView style={styles.container}>
+            <ScrollView contentContainerStyle={styles.scrollViewContent}>
+                <Text style={styles.title}>Profile</Text>
+                <View style={{ alignItems: "flex-end", marginRight: 20, position: "absolute", right: 10, top: 10 }}>
+                    <Button
+                        mode="contained-tonal"
+                        onPress={() => {
+                            setEditButtonPressed(!editButtonPressed);
+                        }}
+                    >
+                        {editButtonPressed ? "Go back" : "Edit"}
+                    </Button>
+                </View>
+                {editButtonPressed ? null : (
+                    <View style={{ alignItems: "center" }}>
+                        <ImageViewer imgSource={studentInfo?.photoUrl || businessInfo?.photoUrl || placeHolderImage}></ImageViewer>
+                        <IconButton icon="camera" size={20} onPress={() => setOpenAvatarPicker(!openAvatarPicker)} />
+                        <AvatarPickingModal open={openAvatarPicker} onClose={() => setOpenAvatarPicker(false)}></AvatarPickingModal>
+                    </View>
+                )}
 
-          <View style={{alignItems: "center"}}>
-          <ImageViewer imgSource={studentInfo?.photoUrl || businessInfo?.photoUrl || placeHolderImage} ></ImageViewer>
-          <IconButton icon="camera" size={20} onPress={()=>setOpenAvatarPicker(!openAvatarPicker)} />
-          <AvatarPickingModal open={openAvatarPicker} onClose={()=> setOpenAvatarPicker(false)}></AvatarPickingModal>
-          </View>}
-
-          {editButtonPressed ? businessInfo && <EditableBusinessInfo businessInfo={businessInfo}/> 
-          : businessInfo && <ReadonlyBusinessInfo businessInfo={businessInfo}/>}
-          {editButtonPressed? studentInfo && <EditableStudentInfo studentInfo={studentInfo}/> 
-          : studentInfo && <ReadonlyStudentInfo studentInfo={studentInfo}/>}
-          {editButtonPressed? null:
-          <View style={styles.buttonContainer}>
-          <Button style={{ margin: 5 }}
-              mode="contained-tonal"
-              onPress={()=>{ setOpenChangePassword(true)}}>
-              Change Password
-            </Button>
-            <ChangePasswordModal open={openChangePassword} onClose={()=> setOpenChangePassword(false)} onChangePassword={handleChangePassword}></ChangePasswordModal>
-            <Button  style={{ margin: 5 }}
-              mode="contained-tonal"
-              onPress={handleLogout}
-            >
-              Log out
-            </Button>
-          </View>}
-        </ScrollView>
-      </SafeAreaView>
+                {editButtonPressed
+                    ? businessInfo && <EditableBusinessInfo businessInfo={businessInfo} />
+                    : businessInfo && <ReadonlyBusinessInfo businessInfo={businessInfo} />}
+                {editButtonPressed
+                    ? studentInfo && <EditableStudentInfo studentInfo={studentInfo} />
+                    : studentInfo && <ReadonlyStudentInfo studentInfo={studentInfo} />}
+                {editButtonPressed ? null : (
+                    <View style={styles.buttonContainer}>
+                        <Button
+                            style={{ margin: 5 }}
+                            mode="contained-tonal"
+                            onPress={() => {
+                                setOpenChangePassword(true);
+                            }}
+                        >
+                            Change Password
+                        </Button>
+                        <ChangePasswordModal
+                            open={openChangePassword}
+                            onClose={() => setOpenChangePassword(false)}
+                            onChangePassword={handleChangePassword}
+                        ></ChangePasswordModal>
+                        <Button style={{ margin: 5 }} mode="contained-tonal" onPress={handleLogout}>
+                            Log out
+                        </Button>
+                    </View>
+                )}
+            </ScrollView>
+        </SafeAreaView>
     );
-    
 }
-
--- components/account/ReadonlyStudentInfo.tsx
+++ components/account/ReadonlyStudentInfo.tsx
@@ -1,41 +1,48 @@
 import { Button, IconButton, Text, TextInput } from "react-native-paper";
-import ImageViewer from "../imageViewer";
+import ImageViewer from "../expoComponents/imageViewer";
 import { useEffect, useState } from "react";
 import { StyleSheet, Platform, View } from "react-native";
 import AvatarPickingModal from "@/modal/AvatarPickingModal";
 import styles from "@/app/styles";
 
-export function ReadonlyStudentInfo({studentInfo}: StudentProps) {
-  const [open, setOpen] = useState<boolean>(false)
+export function ReadonlyStudentInfo({ studentInfo }: StudentProps) {
+    const [open, setOpen] = useState<boolean>(false);
 
-  const placeHolderImage = require("@/assets/images/background-image.png");
+    const placeHolderImage = require("@/assets/images/background-image.png");
 
-  if (studentInfo)
-  return (
-    <View style={styles.container}>
-
-      <Text variant="titleMedium" style={{ marginHorizontal: 10}}>Personal Statement:</Text>
-      <Text variant="titleSmall" style={styles.data}>
-        {studentInfo.personalStatement}
-      </Text>
-      <Text variant="titleMedium" style={{ marginHorizontal: 10}}>Experience:</Text>
-      <Text variant="titleSmall" style={styles.data}>
-        {studentInfo.experience}
-      </Text>
-      <Text variant="titleMedium" style={{ marginHorizontal: 10}}>Email:</Text>
-      <Text variant="titleSmall" style={styles.data}>
-        {studentInfo.email}
-      </Text>
-      <Text variant="titleMedium" style={{ marginHorizontal: 10}}>County:</Text>
-      <Text variant="titleSmall" style={styles.data}>
-        {studentInfo.county}
-      </Text>
-      <Text variant="titleMedium" style={{ marginHorizontal: 10}}>Subjects:</Text>
-      <Text variant="titleSmall" style={styles.data}>
-        {studentInfo.subjects.join(", ")}
-      </Text>
-    </View>
-  );
+    if (studentInfo)
+        return (
+            <View style={styles.container}>
+                <Text variant="titleMedium" style={{ marginHorizontal: 10 }}>
+                    Personal Statement:
+                </Text>
+                <Text variant="titleSmall" style={styles.data}>
+                    {studentInfo.personalStatement}
+                </Text>
+                <Text variant="titleMedium" style={{ marginHorizontal: 10 }}>
+                    Experience:
+                </Text>
+                <Text variant="titleSmall" style={styles.data}>
+                    {studentInfo.experience}
+                </Text>
+                <Text variant="titleMedium" style={{ marginHorizontal: 10 }}>
+                    Email:
+                </Text>
+                <Text variant="titleSmall" style={styles.data}>
+                    {studentInfo.email}
+                </Text>
+                <Text variant="titleMedium" style={{ marginHorizontal: 10 }}>
+                    County:
+                </Text>
+                <Text variant="titleSmall" style={styles.data}>
+                    {studentInfo.county}
+                </Text>
+                <Text variant="titleMedium" style={{ marginHorizontal: 10 }}>
+                    Subjects:
+                </Text>
+                <Text variant="titleSmall" style={styles.data}>
+                    {studentInfo.subjects.join(", ")}
+                </Text>
+            </View>
+        );
 }
-
-
