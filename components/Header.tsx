874e05acbb0cb9fe4873514d9f724f11037a0287
lawaffy <laurawathen@hotmail.com>
2025-03-08 09:01:40 +0000

    updated header styling

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,33 +1,38 @@
 import { Appbar } from "react-native-paper";
 import { useRouter } from "expo-router";
+import { useTheme } from "react-native-paper";
 import styles from "@/app/styles";
 import { TouchableOpacity } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const Header = () => {
-    const router = useRouter();
+  const router = useRouter();
+  const { colors } = useTheme();
 
-    const handleTitlePress = () => {
-        router.push("/(tabs)/(explore)");
-    };
+  const handleTitlePress = () => {
+    router.push("/(tabs)/(explore)");
+  };
 
-    return (
-        <SafeAreaView>
-            <Appbar.Header style={styles.headerContainer}>
-                <TouchableOpacity onPress={handleTitlePress}>
-                    <Appbar.Content
-                        title="Work-XP"
-                        titleStyle={{
-                            fontSize: 18,
-                            alignItems: "center",
-                            padding: 0,
-                            flex: 2,
-                        }}
-                    />
-                </TouchableOpacity>
-            </Appbar.Header>
-        </SafeAreaView>
-    );
+  return (
+    <SafeAreaView>
+      <Appbar.Header
+        style={[styles.headerContainer, { backgroundColor: colors.secondary }]}
+      >
+        <TouchableOpacity onPress={handleTitlePress}>
+          <Appbar.Content
+            title="Work-XP"
+            titleStyle={{
+              fontSize: 18,
+              alignItems: "center",
+              padding: 0,
+              flex: 2,
+              color: colors.tertiary,
+            }}
+          />
+        </TouchableOpacity>
+      </Appbar.Header>
+    </SafeAreaView>
+  );
 };
 
 export default Header;
