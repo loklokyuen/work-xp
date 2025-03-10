f3d5d4c0dedda63ba09af3e8f2ceb8f0dc0c9b6d
lawaffy <laurawathen@hotmail.com>
2025-03-10 10:49:06 +0000

    styling to opps page

--- app/(tabs)/opportunity/index.tsx
+++ app/(tabs)/opportunity/index.tsx
@@ -1,121 +1,156 @@
 import { useState, useEffect } from "react";
 import { useUserContext } from "../../../context/UserContext";
 import { View, ScrollView } from "react-native";
-import { Text, Button } from "react-native-paper";
+import { Text, Button, useTheme } from "react-native-paper";
 import { doc, onSnapshot, collection, deleteDoc } from "firebase/firestore";
 import { db } from "../../../database/firebase";
 import { StyleSheet } from "react-native";
 import { router } from "expo-router";
 
 export default function Opportunities() {
-    const { user } = useUserContext();
-    const [opportunities, setOpportunities] = useState<any[]>([]);
+  const { user } = useUserContext();
+  const [opportunities, setOpportunities] = useState<any[]>([]);
+  const { colors, fonts } = useTheme();
 
-    useEffect(() => {
-        if (user?.uid) {
-            const collectionRef = collection(db, "Business", user.uid, "Opportunities");
-            const unsubscribe = onSnapshot(collectionRef, (snapshot) => {
-                setOpportunities(
-                    snapshot.docs.map((doc) => {
-                        return { id: doc.id, ...doc.data() };
-                    })
-                );
-            });
-            return () => {
-                setOpportunities([]);
-                unsubscribe();
-            };
-        }
-    }, [user?.uid]);
+  useEffect(() => {
+    if (user?.uid) {
+      const collectionRef = collection(
+        db,
+        "Business",
+        user.uid,
+        "Opportunities"
+      );
+      const unsubscribe = onSnapshot(collectionRef, (snapshot) => {
+        setOpportunities(
+          snapshot.docs.map((doc) => {
+            return { id: doc.id, ...doc.data() };
+          })
+        );
+      });
+      return () => {
+        setOpportunities([]);
+        unsubscribe();
+      };
+    }
+  }, [user?.uid]);
 
-    const handleDelete = async (id: string) => {
-        try {
-            if (user) await deleteDoc(doc(db, "Business", user.uid, "Opportunities", id));
-        } catch (err) {
-            console.log(err);
-        }
-    };
+  const handleDelete = async (id: string) => {
+    try {
+      if (user)
+        await deleteDoc(doc(db, "Business", user.uid, "Opportunities", id));
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
-    return (
-        <ScrollView>
-            <View>
-                {opportunities.length === 0 ? (
-                    <Text variant="bodyMedium" style={{ padding: 20, textAlign: "center" }}>
-                        You have not posted any opportunity yet.
-                    </Text>
-                ) : (
-                    opportunities.map((opp, index) => {
-                        return (
-                            <View style={styles.card} key={index}>
-                                <Text style={styles.role}>{opp.jobRole}</Text>
-                                <Text style={styles.description}>{opp.description}</Text>
-                                <Button
-                                    onPress={() => {
-                                        router.push({
-                                            pathname: "/(tabs)/opportunity/listing",
-                                            params: { listingId: opp.id },
-                                        });
-                                    }}
-                                >
-                                    Edit Listing
-                                </Button>
-                                <Button onPress={() => handleDelete(opp.id)}>Delete Listing</Button>
-                                <Button
-                                    onPress={() => {
-                                        router.push({
-                                            pathname: "/(tabs)/opportunity/applications",
-                                            params: { id: opp.id },
-                                        });
-                                    }}
-                                >
-                                    View Applications
-                                </Button>
-                            </View>
-                        );
-                    })
-                )}
-            </View>
-            <Button
-                mode="contained"
-                style={{ marginHorizontal: 20 }}
-                onPress={() => {
+  return (
+    <ScrollView>
+      <View>
+        {opportunities.length === 0 ? (
+          <Text
+            variant="bodyMedium"
+            style={{
+              padding: 20,
+              textAlign: "center",
+              fontFamily: "SpaceMono",
+            }}
+          >
+            You have not posted any opportunity yet.
+          </Text>
+        ) : (
+          opportunities.map((opp, index) => {
+            return (
+              <View style={styles.card} key={index}>
+                <Text
+                  style={StyleSheet.compose(styles.role, {
+                    fontFamily: "SpaceMono",
+                    color: colors.primary,
+                  })}
+                >
+                  {opp.jobRole}
+                </Text>
+                <Text
+                  style={StyleSheet.compose(styles.description, {
+                    fontFamily: "SpaceMono",
+                  })}
+                >
+                  {opp.description}
+                </Text>
+                <Button
+                  labelStyle={{ fontFamily: "SpaceMono" }}
+                  onPress={() => {
                     router.push({
-                        pathname: "/(tabs)/opportunity/listing",
-                        params: { listingId: "" },
+                      pathname: "/(tabs)/opportunity/listing",
+                      params: { listingId: opp.id },
                     });
-                }}
-            >
-                Post Listing
-            </Button>
-        </ScrollView>
-    );
+                  }}
+                >
+                  Edit Listing
+                </Button>
+                <Button
+                  labelStyle={{ fontFamily: "SpaceMono" }}
+                  onPress={() => handleDelete(opp.id)}
+                >
+                  Delete Listing
+                </Button>
+                <Button
+                  labelStyle={{ fontFamily: "SpaceMono" }}
+                  onPress={() => {
+                    router.push({
+                      pathname: "/(tabs)/opportunity/applications",
+                      params: { id: opp.id },
+                    });
+                  }}
+                >
+                  View Applications
+                </Button>
+              </View>
+            );
+          })
+        )}
+      </View>
+      <Button
+        mode="contained"
+        labelStyle={{ fontFamily: "SpaceMono" }}
+        style={{ marginHorizontal: 20 }}
+        onPress={() => {
+          router.push({
+            pathname: "/(tabs)/opportunity/listing",
+            params: { listingId: "" },
+          });
+        }}
+      >
+        Post Listing
+      </Button>
+    </ScrollView>
+  );
 }
 
 const styles = StyleSheet.create({
-    card: {
-        backgroundColor: "#fff",
-        padding: 16,
-        borderRadius: 10,
-        shadowColor: "#000",
-        shadowOpacity: 0.1,
-        shadowOffset: { width: 0, height: 2 },
-        shadowRadius: 4,
-        elevation: 3,
-        marginBottom: 12,
-    },
-    role: {
-        fontSize: 18,
-        fontWeight: "bold",
-        marginBottom: 8,
-    },
-    description: {
-        fontSize: 14,
-        color: "#555",
-        marginBottom: 6,
-    },
-    availability: {
-        fontSize: 12,
-        color: "#007bff",
-        fontWeight: "600",
-    },
+  card: {
+    backgroundColor: "#fff",
+    padding: 16,
+    borderRadius: 10,
+    shadowColor: "#000",
+    shadowOpacity: 0.1,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 4,
+    elevation: 3,
+    marginBottom: 12,
+  },
+  role: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 8,
+  },
+  description: {
+    fontSize: 14,
+    color: "#555",
+    marginBottom: 6,
+  },
+  availability: {
+    fontSize: 12,
+    color: "#007bff",
+    fontWeight: "600",
+  },
 });
