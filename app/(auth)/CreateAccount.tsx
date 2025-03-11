d921ea7e40fcbf08ce3b322f4b1745831467d1cb
Amy <amy_hanwell@hotmail.com>
2025-03-11 12:47:55 +0000

    fix: updates styling for logins

--- app/(auth)/CreateAccount.tsx
+++ app/(auth)/CreateAccount.tsx
@@ -1,9 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { View } from "react-native";
-import { Button, Text, TextInput } from "react-native-paper";
+import { Button, Text, TextInput, useTheme } from "react-native-paper";
 import styles from "../styles";
 
-import { createUserWithEmailAndPassword, sendEmailVerification, updateProfile } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  sendEmailVerification,
+  updateProfile,
+} from "firebase/auth";
 import { auth, db } from "@/database/firebase";
 import { addNewUser } from "@/database/user";
 import { useUserContext } from "@/context/UserContext";
@@ -12,100 +16,217 @@ import { setDoc, doc } from "firebase/firestore";
 import { router } from "expo-router";
 
 const CreateAccount = () => {
-    const { user, setUser, accountType, setAccountType } = useUserContext();
-    const [displayName, setDisplayName] = useState<string>("");
-    const [email, setEmail] = useState<string>("");
-    const [password, setPassword] = useState<string>("");
-    const [error, setError] = useState<string>("");
+  const { user, setUser, accountType, setAccountType } = useUserContext();
+  const [displayName, setDisplayName] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [error, setError] = useState<string>("");
+  const { colors, fonts } = useTheme();
 
-    const handleCreateAccount = async () => {
-        if (!email || !password || !displayName) {
-            setError("Please fill out all fields.");
-            return;
+  const handleCreateAccount = async () => {
+    if (!email || !password || !displayName) {
+      setError("Please fill out all fields.");
+      return;
+    }
+    if (accountType) {
+      try {
+        const userCredential = await createUserWithEmailAndPassword(
+          auth,
+          email,
+          password
+        );
+        const user = userCredential.user;
+        const userData: User = {
+          uid: user.uid,
+          displayName: displayName,
+          email: email,
+          photoUrl: "",
+        };
+        await updateProfile(user, {
+          displayName: displayName,
+        });
+        await addNewUser(
+          user.uid,
+          accountType,
+          displayName || "",
+          user.photoURL || "",
+          email || ""
+        );
+        await setDoc(doc(db, "Users", user.uid), {
+          accountType: accountType,
+        });
+        setUser(userData);
+        setAccountType(accountType);
+        setError("");
+        router.replace("/(tabs)");
+        // sendEmailVerification(user)
+        //   .then(() => {
+        //     alert("Email verification sent.");
+        //   })
+        //   .catch((error) => {
+        //     console.error("Error sending email verification:", error);
+        //   });
+      } catch (error) {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        setError(errorMessage);
+        if (errorCode === "auth/email-already-in-use") {
+          setError("Email already in use.");
         }
-        if (accountType) {
-            try {
-                const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-                const user = userCredential.user;
-                const userData: User = {
-                    uid: user.uid,
-                    displayName: displayName,
-                    email: email,
-                    photoUrl: "",
-                };
-                await updateProfile(user, {
-                    displayName: displayName,
-                });
-                await addNewUser(user.uid, accountType, displayName || "", user.photoURL || "", email || "");
-                await setDoc(doc(db, "Users", user.uid), {
-                    accountType: accountType,
-                });
-                setUser(userData);
-                setAccountType(accountType);
-                setError("");
-                router.replace("/(tabs)");
-                // sendEmailVerification(user)
-                //   .then(() => {
-                //     alert("Email verification sent.");
-                //   })
-                //   .catch((error) => {
-                //     console.error("Error sending email verification:", error);
-                //   });
-            } catch (error) {
-                const errorCode = error.code;
-                const errorMessage = error.message;
-                setError(errorMessage);
-                if (errorCode === "auth/email-already-in-use") {
-                    setError("Email already in use.");
-                }
-                if (errorCode === "auth/weak-password") {
-                    setError("Password should include at least an upper case letter, a lower case letter and needs to be at least 6 digits.");
-                }
-            }
-        } else {
-            setError("Please choose your account type.");
+        if (errorCode === "auth/weak-password") {
+          setError(
+            "Password should include at least an upper case letter, a lower case letter and needs to be at least 6 digits."
+          );
         }
-    };
+      }
+    } else {
+      setError("Please choose your account type.");
+    }
+  };
 
-    return (
-        <View style={styles.container}>
-            <Text style={styles.title}>Create {accountType} Account</Text>
-            <View style={styles.buttonContainer}>
-                <Button mode="contained-tonal" onPress={() => setAccountType("Student")}>
-                    Student
-                </Button>
-                <Text variant="titleMedium" style={{ textAlign: "center", alignContent: "center" }}>
-                    {" "}
-                    OR
-                </Text>
-                <Button mode="contained-tonal" onPress={() => setAccountType("Business")}>
-                    Business
-                </Button>
-            </View>
-            <TextInput style={styles.input} label="Display Name" value={displayName} onChangeText={setDisplayName} autoCapitalize="words" />
-            <TextInput style={styles.input} label="Email" value={email} onChangeText={setEmail} keyboardType="email-address" autoCapitalize="none" />
-            <TextInput style={styles.input} label="Password" value={password} onChangeText={setPassword} secureTextEntry />
-            {error ? <Text style={styles.error}>{error}</Text> : null}
-            <View style={styles.buttonContainer}>
-                <Button mode="contained-tonal" onPress={handleCreateAccount}>
-                    Create Account
-                </Button>
-            </View>
-            <Text variant="titleMedium" style={{ textAlign: "center", margin: 10 }}>
-                Already have an account?
-            </Text>
-            <View style={styles.buttonContainer}>
-                <Button
-                    mode="outlined"
-                    onPress={() => {
-                        router.replace("/SignIn");
-                    }}
-                >
-                    Sign in
-                </Button>
-            </View>
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <Text
+        style={[styles.title, { color: colors.primary, ...fonts.titleLarge }]}
+      >
+        Create {accountType} Account
+      </Text>
+      <View style={styles.buttonContainer}>
+        <Button
+          style={{
+            backgroundColor: colors.secondary,
+            borderRadius: 8,
+            paddingLeft: 5,
+            paddingRight: 5,
+            marginBottom: 15,
+          }}
+          labelStyle={{
+            fontFamily: "SpaceMono",
+            fontSize: 16,
+            fontWeight: "normal",
+            color: colors.tertiary,
+          }}
+          //   mode="contained-tonal"
+          onPress={() => setAccountType("Student")}
+        >
+          Student 🎒
+        </Button>
+        <Text variant="titleMedium" style={{ margin: 10, color: "#3E92CC" }}>
+          or
+        </Text>
+        <Button
+          style={{
+            backgroundColor: colors.secondary,
+            borderRadius: 8,
+            paddingLeft: 5,
+            paddingRight: 5,
+            marginBottom: 15,
+          }}
+          labelStyle={{
+            fontFamily: "SpaceMono",
+            fontSize: 16,
+            fontWeight: "normal",
+            color: colors.tertiary,
+          }}
+          //   mode="contained-tonal"
+          onPress={() => setAccountType("Business")}
+        >
+          Business 🏢
+        </Button>
+      </View>
+      <TextInput
+        style={[
+          styles.input,
+          {
+            backgroundColor: colors.primaryContainer,
+            color: colors.primary,
+            ...fonts.bodyMedium,
+          },
+        ]}
+        label="Display Name"
+        value={displayName}
+        onChangeText={setDisplayName}
+        autoCapitalize="words"
+      />
+      <TextInput
+        style={[
+          styles.input,
+          {
+            backgroundColor: colors.primaryContainer,
+            color: colors.primary,
+            ...fonts.bodyMedium,
+          },
+        ]}
+        label="Email"
+        value={email}
+        onChangeText={setEmail}
+        keyboardType="email-address"
+        autoCapitalize="none"
+      />
+      <TextInput
+       style={[
+        styles.input,
+        {
+          backgroundColor: colors.primaryContainer,
+          color: colors.primary,
+          ...fonts.bodyMedium,
+        },
+      ]} 
+        label="Password"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+      />
+      {error ? <Text style={styles.error}>{error}</Text> : null}
+      <View style={styles.buttonContainer}>
+      <Button 
+          style={{
+            backgroundColor: colors.secondary, 
+            borderRadius: 8, 
+            paddingLeft: 5,
+            paddingRight: 5,
+            marginBottom: 15,
+            marginTop: 10,
+          }}
+          labelStyle={{
+            fontFamily: "SpaceMono", 
+            fontSize: 16, 
+            fontWeight: "normal", 
+            color: colors.tertiary, 
+          }}
+        mode="contained-tonal" 
+        onPress={handleCreateAccount}>
+          Create Account
+        </Button>
+      </View>
+      <Text variant="titleMedium" style={{ margin: 20, color: "#3E92CC" }}>
+        Already have an account?
+      </Text>
+      <View style={styles.buttonContainer}>
+      <Button
+          style={{
+            backgroundColor: colors.secondary, 
+            borderRadius: 8, 
+            paddingLeft: 5,
+            paddingRight: 5,
+            marginBottom: 15,
+          }}
+          labelStyle={{
+            fontFamily: "SpaceMono", 
+            fontSize: 16, 
+            fontWeight: "normal", 
+            color: colors.tertiary, 
+          }}
+          mode="contained-tonal" 
+          onPress={() => {
+            router.replace("/SignIn");
+          }}
+        >
+          Sign in
+        </Button>
+      </View>
+    </View>
+  );
 };
 
 export default CreateAccount;
--- app/(auth)/main.tsx
+++ app/(auth)/main.tsx
@@ -1,82 +1,145 @@
 import { setUserAccountType, useUserContext } from "@/context/UserContext";
 
 import { View } from "react-native";
-import { Button, Text } from "react-native-paper";
+import { Button, Text, useTheme } from "react-native-paper";
 import { useState } from "react";
 
 import { auth } from "@/database/firebase";
 import { signInAnonymously } from "firebase/auth";
 import { router } from "expo-router";
 
+import styles from "../styles";
+
 export default function AccountScreen() {
-    const { user, setUser, setAccountType } = useUserContext();
-    const [error, setError] = useState<string>("");
+  const { user, setUser, setAccountType } = useUserContext();
+  const [error, setError] = useState<string>("");
+  const { colors, fonts } = useTheme();
 
-    const handleGuestSignIn = () => {
-        signInAnonymously(auth)
-            .then((userCredential) => {
-                const user = userCredential.user;
-                setUser({
-                    uid: user.uid,
-                    displayName: "Guest",
-                    email: "",
-                    photoUrl: "",
-                });
-                setUserAccountType("Guest");
-                setAccountType("Guest");
-                setError("");
-                router.replace("/(tabs)");
-            })
-            .catch((error) => {
-                const errorCode = error.code;
-                const errorMessage = error.message;
-                setError(errorMessage);
-            });
-    };
+  const handleGuestSignIn = () => {
+    signInAnonymously(auth)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        setUser({
+          uid: user.uid,
+          displayName: "Guest",
+          email: "",
+          photoUrl: "",
+        });
+        setUserAccountType("Guest");
+        setAccountType("Guest");
+        setError("");
+        router.replace("/(tabs)");
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        setError(errorMessage);
+      });
+  };
 
-    return (
-        <View style={{ backgroundColor: "#FFFAFF", flex: 1, justifyContent: "center", alignItems: "center" }}>
-            <Text variant="titleLarge" style={{ margin: 10, color: "#3E92CC" }}>
-                Welcome to Work-XP!
-            </Text>
-            <Text variant="titleMedium" style={{ margin: 20, color: "#07070A", textAlign: "center" }}>
-                Find the work experience opportunity that's perfect for you, from the comfort of your phone!
-            </Text>
-            <Text variant="titleMedium" style={{ margin: 10, color: "#3E92CC" }}>
-                New to the app?
-            </Text>
-            <Button
-                mode="contained-tonal"
-                style={{ margin: 10 }}
-                onPress={() => {
-                    router.replace("/CreateAccount");
-                }}
-            >
-                Create an account
-            </Button>
-            <Text variant="titleMedium" style={{ margin: 10, color: "#3E92CC" }}>
-                Already have an account?
-            </Text>
-            <Button
-                mode="contained-tonal"
-                style={{ margin: 10 }}
-                onPress={() => {
-                    router.replace("/SignIn");
-                }}
-            >
-                Sign in to your account
-            </Button>
-            <Text variant="titleMedium" style={{ margin: 10, color: "#3E92CC" }}>
-                Or just want to browse?
-            </Text>
-            <Button mode="outlined" style={{ margin: 10 }} onPress={handleGuestSignIn}>
-                Sign in as guest
-            </Button>
-            {error ? (
-                <Text variant="titleSmall" style={{ color: "red" }}>
-                    {error}
-                </Text>
-            ) : null}
-        </View>
-    );
+  return (
+    <View
+      style={{
+        backgroundColor: "#FFFAFF",
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+     <Text style={[styles.title, { color: colors.primary, ...fonts.titleLarge }]}>
+        Welcome to Work-XP!
+      </Text>
+      <Text
+           style={[styles.body, { color: colors.tertiary, ...fonts.bodyMedium}]}
+      >
+        Find the work experience opportunity that's perfect for you, from the
+        comfort of your phone! 📲
+      </Text>
+      <Text style={[styles.subTitle, { color: colors.quarternary, ...fonts.titleMedium}]}>
+        New to the app?
+      </Text>
+      <Button
+        // mode="contained-tonal"
+        // style={{ margin: 10 }}
+        style={{
+            backgroundColor: colors.secondary, // Background color
+            borderRadius: 8, // Optional: For rounded edges
+            paddingLeft: 5,
+            paddingRight: 5,
+            marginBottom: 15,
+          }}
+          labelStyle={{
+            fontFamily: "SpaceMono", // Apply custom font
+            fontSize: 16, // Adjust font size as needed
+            fontWeight: "normal", // Set font weight
+            color: colors.tertiary, // Text color (onPrimary works well for contrast)
+          }}
+        onPress={() => {
+          router.replace("/CreateAccount");
+        }}
+      >
+        Create an account
+      </Button>
+      <Text style={[styles.subTitle, { color: colors.quarternary, ...fonts.titleMedium}]}>
+        Already have an account?
+      </Text>
+      <Button
+        // mode="contained-tonal"
+        // style={{ margin: 10 }}
+        style={{
+            backgroundColor: colors.secondary, // Background color
+            borderRadius: 8, // Optional: For rounded edges
+            paddingLeft: 5,
+            paddingRight: 5,
+            marginBottom: 15,
+          }}
+          labelStyle={{
+            fontFamily: "SpaceMono", // Apply custom font
+            fontSize: 16, // Adjust font size as needed
+            fontWeight: "normal", // Set font weight
+            color: colors.tertiary, // Text color (onPrimary works well for contrast)
+          }}
+        onPress={() => {
+          router.replace("/SignIn");
+        }}
+      >
+        Sign in
+      </Button>
+      <Text style={[styles.subTitle, { color: colors.quarternary, ...fonts.titleMedium}]}>
+        Or just want to browse?
+      </Text>
+      <Button
+        mode="outlined"
+        // style={{ margin: 10 }}
+        style={{
+            backgroundColor: colors.primaryContainer,
+            paddingVertical: 2, // Adds vertical padding for height
+            borderRadius: 8, // Makes the box rounded (adjust as needed)
+            justifyContent: "center", // Centers the text vertically
+            alignItems: "center", // Centers the text horizontally
+            borderWidth: 0,
+          }}
+        onPress={handleGuestSignIn}
+      >
+        <Text
+            style={[
+              styles.option,
+              {
+                fontFamily: "SpaceMono", // Apply custom font
+                fontSize: 15, // Adjust font size as needed
+                color: colors.primary, // Set text color to primary or other color
+                textAlign: "center", // Centers the text horizontally
+              },
+            ]}
+          >
+            Sign in as guest
+          </Text>
+      </Button>
+      {error ? (
+        <Text variant="titleSmall" style={{ color: "red" }}>
+          {error}
+        </Text>
+      ) : null}
+    </View>
+  );
 }
