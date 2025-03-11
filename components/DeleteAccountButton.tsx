217aebf73bafd9f7b807db6c3148fc453b812d8d
matthewh900 <matthewh900@gmail.com>
2025-03-11 10:05:32 +0000

    add logic to delete button to check account type

--- components/DeleteAccountButton.tsx
+++ components/DeleteAccountButton.tsx
@@ -1,37 +1,63 @@
-import { deleteApplicationsById, getApplications } from "@/database/applications"
-import { Button } from "react-native-paper"
-import { useUserContext } from "@/context/UserContext"
-import { deleteStudentById } from "@/database/student"
-import { ConfirmActionModal } from "@/modal/ConfirmActionModal"
-import { useState } from "react"
+import {
+  deleteApplicationsById,
+  getApplications,
+} from "@/database/applications";
+import { Button } from "react-native-paper";
+import { useUserContext } from "@/context/UserContext";
+import { deleteStudentById } from "@/database/student";
+import { ConfirmActionModal } from "@/modal/ConfirmActionModal";
+import { useState } from "react";
+import { deleteBusinessById } from "@/database/business";
 
 export function DeleteAccountButton() {
-    const {user} = useUserContext()
-    const [openDelete, setOpenDelete] = useState<boolean>(false)
+  const { user, accountType } = useUserContext();
+  const [openDelete, setOpenDelete] = useState<boolean>(false);
 
-    const handleDelete = () => {
-        getApplications().then((res) => {
-            res.map((application) => {
-                if(application.studentId === user?.uid){
-                    deleteApplicationsById(application.uid)
-                }
-            })
-        }).then(() => {
-            deleteStudentById(user.uid)
-        }).catch((err) => {
-            console.log(err)
+  const handleDelete = () => {
+    if (accountType === "Student") {
+      getApplications()
+        .then((res) => {
+          res.map((application) => {
+            if (application.studentId === user?.uid) {
+              deleteApplicationsById(application.uid);
+            }
+          });
         })
+        .then(() => {
+          deleteStudentById(user.uid);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } else if (accountType === "Business") {
+      getApplications()
+        .then((res) => {
+          res.map((application) => {
+            if (application.businessId === user?.uid) {
+              deleteApplicationsById(application.uid);
+            }
+          });
+        })
+        .then(() => {
+          deleteBusinessById(user.uid);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     }
+  };
 
-    return (
-        <>
-        <Button mode="contained-tonal" onPress={() => setOpenDelete(true)}>Delete Account</Button>
-        <ConfirmActionModal 
+  return (
+    <>
+      <Button mode="contained-tonal" onPress={() => setOpenDelete(true)}>
+        Delete Account
+      </Button>
+      <ConfirmActionModal
         open={openDelete}
         onClose={() => setOpenDelete(false)}
         title="Are you sure?"
         onConfirmAction={handleDelete}
-        />
-        </>
-    )
+      />
+    </>
+  );
 }
